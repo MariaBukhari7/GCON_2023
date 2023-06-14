4b21a08ea162d17b7a4c813ab217f6e8b288b00e
MariaBukhari7 <106581243+MariaBukhari7@users.noreply.github.com>
2023-06-15 02:09:10 +0300

    DONE

--- GCON_SheCan/Assets/GamePlayScene.cs
+++ GCON_SheCan/Assets/GamePlayScene.cs
@@ -5,11 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class GamePlayScene : MonoBehaviour
 {
-
+    [SerializeField] Texture2D png;
+    [SerializeField] Vector2 _hotspot = Vector2.zero;
+    [SerializeField] CursorMode cursorMode = CursorMode.Auto;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.SetCursor(png, _hotspot, cursorMode);
+
     }
 
     // Update is called once per frame
@@ -21,6 +24,12 @@ public class GamePlayScene : MonoBehaviour
     public void GamePlay() 
     {
         SceneManager.LoadScene("SheCan_GamePlay_Scene");
+
+
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("SheCan_MainMenu_Scene");
 
 
     }
